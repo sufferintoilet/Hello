b48b956bbc3b9e7282d1f24ee70ec3b373eb79c0
sufferintoilet <nguyenhuynhquocan1@gmail.com>
2023-12-07 00:42:58 +0900

    My name

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your name: An");
+        string yourName = Console.ReadLine();
+        Console.WriteLine("Hello: " + yourName);
+    }
+
+}
